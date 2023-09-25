e3f62c17f2387953cd667b8072efa2b78580fc86
kimemia21 <bobbymbogo71@gmail.com>
2023-09-25 17:20:11 +0300

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,15 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response =requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data=json.loads(self.get_response_body())
+        
+        return data
+    
+
+
+instance =GetRequester(" https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+print(instance.load_json())    
